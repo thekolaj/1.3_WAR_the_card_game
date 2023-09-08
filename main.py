d553b6a4e1e0dbcec48593b0fe9c995bb8a9e7b3
s p <prietosebby@gmail.com>
2023-09-08 11:53:09 +0300

    step 1 complete

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import random
 import sys
 
-def main():
-    Game.main_loop()
-
+def main(): 
+    game = Game()
+    print(game.deck)
+    
 class Game:
-    @classmethod
-    def main_loop():
-        ...
+    def __init__(self):
+        self.deck = []
+        for i in range(2,15):
+            self.deck.extend([Card(i, "♥"),Card(i, "♠"),Card(i, "♦"),Card(i, "♣")])
+
+    # ...(♣), diamonds (♦), hearts (♥) and spades (♠)
 
 class Player:
     def __init__(self, deck: list, name: str) -> None:
@@ -17,6 +21,26 @@ class Player:
 
     def shuffle():
         ...
+        
+class Card:
+    cards = {11: "J", 12: "Q", 13:"K", 14: "A"}
+    for low_cards in range(2,11):
+        cards[low_cards]=str(low_cards)
+        
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+        
+    def __eq__(self, other):
+        return self.value == other.value
+    
+    def __lt__(self, other):
+        return self.value < other.value
+    
+    def __repr__(self):
+        return self.cards[self.value] +" "+ self.suit
+                    
+        
 
 if __name__ == "__main__":
     main()
